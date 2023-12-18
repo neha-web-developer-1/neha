468fbe36a6a8783f044a2f7c62dbef5e33a26e23
nehakumar <neha9213nk@gmail.com>
2023-12-18 12:09:33 +0530

    18th dec

---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+
+
+import sh from './Shizu.webp'
+import './About.css'
+const About = ()=>{
+    return(
+        <>
+        <div className="aboutus">
+        <h1 className='heading'>About our resort</h1>
+        <p>Where can I get some?
+There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+
+5
+	</p>
+    <img src={sh} width={250}/>
+    </div>
+        </>
+    )
+}
+export default About
